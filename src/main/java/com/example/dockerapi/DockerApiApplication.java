2cfc34957cf525d351b6d17a8993fb76f7452125
Nodirbek <no0404ir@gmail.com>
2023-11-25 15:03:53 +0500

    fghj

--- src/main/java/com/example/dockerapi/DockerApiApplication.java
+++ src/main/java/com/example/dockerapi/DockerApiApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DockerApiApplication {
 
+    
     public static void main(String[] args) {
         SpringApplication.run(DockerApiApplication.class, args);
     }
